33fa72b1e1abf62941ca85922b9ac6fa7a76d1a1
Chase Linzmeyer <chaselinzmeyer@MacBook-Pro-5.local>
2019-04-21 11:51:11 -0500

    moved icon library to this file

---
+++ src/icons/icons.js
@@ -0,0 +1,36 @@
+//---------- FONT AWESOME ICONS ------------------------------------------------
+
+// import Font Awesome Library from dependency
+import { library as icons } from '@fortawesome/fontawesome-svg-core'
+
+// import icons from dependencies
+
+
+import { 
+  faStroopwafel, faHome, faCircle, faAnchor, faCheckSquare, faCoffee,
+  faChartPie, faGlobe, faCog, faUsers, faCopyright, faAddressCard, faInfoCircle, faPeopleCarry,
+} from '@fortawesome/free-solid-svg-icons';
+
+import { faAddressBook } from '@fortawesome/free-regular-svg-icons';
+
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
+// add individual icons to library
+icons.add(
+  fab,
+  faStroopwafel,
+  faHome,
+  faCircle,
+  faAnchor,
+  faCheckSquare, 
+  faCoffee,
+  faChartPie,
+  faGlobe,
+  faCog,
+  faUsers,
+  faCopyright,
+  faAddressCard,
+  faInfoCircle,
+  faPeopleCarry,
+  faAddressBook,
+);
