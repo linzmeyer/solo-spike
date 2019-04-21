9ba1091938e86986b4bcb0da3dc06f3411800770
Chase Linzmeyer <chaselinzmeyer@MacBook-Pro-5.local>
2019-04-21 11:12:21 -0500

    moved library to separate file

--- src/index.js
+++ src/index.js
@@ -4,20 +4,8 @@ import './index.css';
 import App from './App/App';
 import * as serviceWorker from './serviceWorker';
 
-//---------- FONT AWESOME ------------------------------------------------------
-
-// import Font Awesome Library
-import { library } from '@fortawesome/fontawesome-svg-core'
-// import Font Awesome Icons
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faStroopwafel } from '@fortawesome/free-solid-svg-icons'
-import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
-
-// add icons to library
-library.add( fab, faStroopwafel, faCheckSquare, faCoffee );
-
-//-------End FONT AWESOME ------------------------------------------------------
-
+// import your library of icons
+import './icons/icons';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
